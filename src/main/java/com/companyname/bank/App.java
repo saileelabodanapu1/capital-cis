060536ee0567bb21156fe422d8f32ea7b57532d8
saileelabodanapu1 <118717711+saileelabodanapu1@users.noreply.github.com>
2022-11-26 01:11:26 +0530

    1

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "github webhook testing ");
+        System.out.println( "github webhook testin1g ");
     }
 }
